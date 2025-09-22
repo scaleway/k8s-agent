a35943b1b3e5d5dddad26f120e4d86886e3ff154
Pablo RUTH <contact@pablo-ruth.fr>
2025-09-22 17:13:18 +0200

    chore: upgrade sprig to v3 (#15)

--- helpers.go
+++ helpers.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 	"text/template"
 
-	"github.com/Masterminds/sprig"
+	"github.com/Masterminds/sprig/v3"
 )
 
 func writeFile(cacheFS fs.FS, name, src, dst, mode, owner, group string) (string, error) {
